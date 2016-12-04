594b21fab576d238fadffa94fd60ba0070bc611a
Andrew Trice <tricead@g.cofc.edu>
2016-12-04 16:21:06 -0500

    Create InsertAtTailOfLL.java

---
+++ InsertAtTailOfLL.java
@@ -0,0 +1,23 @@
+/*
+  Insert Node at the end of a linked list 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+Node Insert(Node head,int data) {
+// This is a "method-only" submission. 
+// You only need to complete this method.
+    
+    Node tail = new Node();
+    tail.data = data;
+    tail.next = null;
+    Node currNode = head;
+    while(currNode.next != null) {
+        currNode = currNode.next;
+    }
+    currNode.next = tail;
+    return head;
+}
